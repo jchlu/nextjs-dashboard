e69e068bcf1b97207960ecadae44f96aa6b573c7
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-10-27 12:19:14 -0400

    Fix nav using Link component

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,7 +4,7 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
-
+import Link from 'next/link'
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -23,7 +23,7 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className={clsx(
@@ -32,7 +32,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         )
       })}
     </>
