f1c8923d0a6a0e01b76f77e61e89f2bcbe851470
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-10-27 12:46:22 -0400

    Navigating Between Pages

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   UserGroupIcon,
   HomeIcon,
@@ -5,6 +6,7 @@ import {
 } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -18,6 +20,7 @@ const links = [
 ]
 
 export default function NavLinks() {
+const pathname = usePathname()
   return (
     <>
       {links.map((link) => {
@@ -27,7 +30,10 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href
+              }
             )}
           >
             <LinkIcon className="w-6" />
