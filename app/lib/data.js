773eaa406bd1d866841b02596ce8079669f70ba1
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-02 15:00:27 -0400

    Cleanup comments

--- app/lib/data.js
+++ app/lib/data.js
@@ -12,17 +12,8 @@ export async function fetchRevenue() {
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
-    // Artificially delay a reponse for demo purposes.
-    // Don't do this in real life :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    // const data = await sql<Revenue>`SELECT * FROM revenue`
-
     let { data: revenue, error } = await supabase.from('revenue').select('*')
 
-    // console.log('Data fetch complete after 3 seconds.');
     return revenue
   } catch (error) {
     console.error('Database Error:', error)
