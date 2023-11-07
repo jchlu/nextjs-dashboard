38d89ffe5d5721553ff2f6bdf9016d677984eac7
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-07 15:32:36 -0400

    Revert "Prototype Clerk Auth"
    This reverts commit 682822e052dedc667608b682dd51cb1291b4bc6e.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,10 @@
-import { ClerkProvider } from '@clerk/nextjs'
 import '@/app/ui/global.css'
 import { geist } from './ui/fonts'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <ClerkProvider
-        afterSignInUrl="/dashboard"
-        appearance={{
-          signIn: {
-            elements: {
-              footer: {
-                display: 'none',
-              },
-            },
-          },
-        }}
-      >
-        <body className={`${geist.className} antialiased`}>{children}</body>
-      </ClerkProvider>
+      <body className={`${geist.className} antialiased`}>{children}</body>
     </html>
   )
 }
