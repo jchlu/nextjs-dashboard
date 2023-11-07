682822e052dedc667608b682dd51cb1291b4bc6e
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-07 13:08:38 -0400

    Prototype Clerk Auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,24 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import '@/app/ui/global.css'
 import { geist } from './ui/fonts'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geist.className} antialiased`}>{children}</body>
+      <ClerkProvider
+        afterSignInUrl="/dashboard"
+        appearance={{
+          signIn: {
+            elements: {
+              footer: {
+                display: 'none',
+              },
+            },
+          },
+        }}
+      >
+        <body className={`${geist.className} antialiased`}>{children}</body>
+      </ClerkProvider>
     </html>
   )
 }
